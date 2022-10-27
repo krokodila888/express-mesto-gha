a86b5ec69289f7b2982ad38415c9004b60fd348c
Evgenia <101995565+krokodila888@users.noreply.github.com>
2022-10-27 23:17:11 +0300

    Add files via upload

--- controllers/users.js
+++ controllers/users.js
@@ -23,11 +23,12 @@ module.exports.getUsers = (req, res) => {
 };*/
 
 module.exports.getUser = (req, res) => {
-  User.findById(req.user._id, { new: false },)
-    .orFail((err) => {return res.status(ERROR_CODE_WRONG_DATA).send({ message: ERROR_MESSAGE.USER_GET_ID_ERROR })})
-    .then((user) => res.send({ data: user }))
+  User.findById(req.user._id)
+    .orFail(() => {return res.status(ERROR_CODE_WRONG_DATA).send({ message: ERROR_MESSAGE.USER_GET_ID_ERROR })})
+    .then((user) => {if (!user) {return res.status(ERROR_CODE_NOT_FOUND).send({ message: ERROR_MESSAGE.USER_GET_ID_ERROR })}
+      res.send({ data: user })})
     .catch((err) => {
-      if(err.message === 'NotFound') {
+      if (err.message === 'NotFound') {
         return res.status(ERROR_CODE_WRONG_DATA).send({ message: ERROR_MESSAGE.USER_GET_ID_ERROR });
       }
       if (res.status(ERROR_CODE_NOT_FOUND)) {
@@ -51,7 +52,7 @@ module.exports.createUser = (req, res) => {
 module.exports.editUserProfile = (req, res) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
-    .then((user) => res.send({ data: user }))
+    .then((user) => {if (!user) {return res.status(ERROR_CODE_NOT_FOUND).send({ message: ERROR_MESSAGE.USER_GET_ID_ERROR })} res.send({ data: user })})
     .catch((err) => {
       if (err.name === 'ValidationError') {
         return res.status(ERROR_CODE_WRONG_DATA).send({ message: ERROR_MESSAGE.USER_PATCH_PROFILE_INVALID_DATA_ERROR });
@@ -69,7 +70,7 @@ module.exports.editUserProfile = (req, res) => {
 module.exports.editUserAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
-    .then((user) => res.send({ data: user }))
+    .then((user) => {if (!user) {return res.status(ERROR_CODE_NOT_FOUND).send({ message: ERROR_MESSAGE.USER_GET_ID_ERROR })} res.send({ data: user })})
     .catch((err) => {
       if (err.name === 'ValidationError') {
         return res.status(ERROR_CODE_WRONG_DATA).send({ message: ERROR_MESSAGE.USER_PATCH_AVATAR_INVALID_DATA_ERROR });
