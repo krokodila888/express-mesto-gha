23a83a9e76297481622ba2614ad70a43642c8988
Evgenia <101995565+krokodila888@users.noreply.github.com>
2022-10-27 23:45:16 +0300

    Add files via upload

--- controllers/users.js
+++ controllers/users.js
@@ -23,21 +23,15 @@ module.exports.getUsers = (req, res) => {
 };*/
 
 module.exports.getUser = (req, res) => {
-  User.findById(req.user._id)
-    //.orFail(() => {return res.status(ERROR_CODE_WRONG_DATA).send({ message: ERROR_MESSAGE.USER_GET_ID_ERROR })})
+  User.findById(req.params.userId)
+    .orFail(() => {return res.status(ERROR_CODE_WRONG_DATA).send({ message: ERROR_MESSAGE.USER_GET_ID_ERROR })})
     .then((user) => {if (!user) {return res.status(ERROR_CODE_NOT_FOUND).send({ message: ERROR_MESSAGE.USER_GET_ID_ERROR })}
       res.send({ data: user })})
     .catch((err) => {
-      if (err.message === 'NotFound') {
-        return res.status(ERROR_CODE_NOT_FOUND).send({ message: ERROR_MESSAGE.USER_GET_ID_ERROR });
+      if(err.message === 'NotFound') {
+        return res.status(ERROR_CODE_WRONG_DATA).send({ message: ERROR_MESSAGE.USER_GET_ID_ERROR });
       }
-      if (err.name === 'ValidationError') {
-        return res.status(ERROR_CODE_WRONG_DATA).send({ message: ERROR_MESSAGE.USER_POST_ERROR });
-      }
-      if (res.status(ERROR_CODE_NOT_FOUND)) {
-        return res.status(ERROR_CODE_NOT_FOUND).send({ message: ERROR_MESSAGE.USER_GET_ID_ERROR });
-      }
-      return res.status(ERROR_CODE_SOMETHING_IS_WRONG).send({ message: ERROR_MESSAGE.SOMETHING_IS_WRONG });
+      return res.status(ERROR_CODE_SOMETHING_IS_WRONG).send({ message: err.message, name: err.name });
     });
 };
 
