8ea204994e6fba1e6bd7d7dd11e948e90d5c6c3c
Evgenia <101995565+krokodila888@users.noreply.github.com>
2022-11-12 00:35:11 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -31,13 +31,14 @@ const auth = require('./middlewares/auth');
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(6),
+    password: Joi.string().required().min(),
   }),
 }), login);
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(6),
+    password: Joi.string().required().min(),
     name: Joi.string().min(2).max(30).default('Жак-Ив Кусто'),
     about: Joi.string().min(2).max(30).default('Исследователь'),
     avatar: Joi.string().pattern(/^https?:\/\/(www\.)?([A-Za-z\d-])+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/).default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'),
