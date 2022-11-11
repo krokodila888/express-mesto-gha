14da4037d1c70e12ce62e22d8b63d0be68ea0ce5
Evgenia <101995565+krokodila888@users.noreply.github.com>
2022-11-12 00:40:06 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -31,14 +31,14 @@ const auth = require('./middlewares/auth');
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(),
+    password: Joi.string().required(),
   }),
 }), login);
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(),
+    password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(/^https?:\/\/(www\.)?([A-Za-z\d-])+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/),
