e94b79e7f2e1eb11d5eef30a591b064668318202
Evgenia <101995565+krokodila888@users.noreply.github.com>
2022-11-12 15:21:21 +0300

    Add files via upload

--- controllers/cards.js
+++ controllers/cards.js
@@ -51,32 +51,42 @@ module.exports.deleteCard = (req, res, next) => {
 };
 
 module.exports.likeCard = (req, res, next) => {
-  Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
-    .orFail(() => {
-      throw new NotFoundError(ERROR_MESSAGE.PUT_LIKE_INV_DATA);
+  Card.findById(req.params.cardId)
+    .then((card) => {
+      if (!card) {
+        throw new NotFoundError(ERROR_MESSAGE.DELETE_LIKE_NO_ID);
+      }
+      Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
+        .orFail(() => {
+          throw new NotFoundError(ERROR_MESSAGE.PUT_LIKE_INV_DATA);
+        })
+        .then((card) => res.send({ data: card }))
+      .catch((err) => {
+        if (err.message === 'NotFound') {
+          next(new NotFoundError(ERROR_MESSAGE.PUT_LIKE_INV_DATA));
+        }
+        if (err.name === 'ValidationError') {
+          next(new RequestError(ERROR_MESSAGE.PUT_LIKE_INV_DATA));
+        }
+        if (err.name === 'CastError') {
+          next(new RequestError(ERROR_MESSAGE.CARD_DEL_WRONG_ID));
+        } else {
+          next(err);
+        }
+      });
     })
-    .then((card) => res.send({ data: card }))
-    .catch((err) => {
-      if (err.message === 'NotFound') {
-        next(new NotFoundError(ERROR_MESSAGE.PUT_LIKE_INV_DATA));
-      }
-      if (err.name === 'ValidationError') {
-        next(new RequestError(ERROR_MESSAGE.PUT_LIKE_INV_DATA));
-      }
-      if (err.name === 'CastError') {
-        next(new RequestError(ERROR_MESSAGE.CARD_DEL_WRONG_ID));
-      } else {
-        next(err);
-      }
-    });
 };
 
 module.exports.deleteLike = (req, res, next) => {
-  Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
-    .orFail(() => {
-      throw new NotFoundError(ERROR_MESSAGE.DELETE_LIKE_NO_ID);
+  Card.findById(req.params.cardId)
+    .then((card) => {
+      if (!card) {
+        throw new NotFoundError(ERROR_MESSAGE.DELETE_LIKE_NO_ID);
+      }
+      Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
+        .then((newCard) => res.send({ data: newCard }))
+        .catch((err) => next(err));
     })
-    .then((card) => res.send({ data: card }))
     .catch((err) => {
       if (err.message === 'NotFound') {
         next(new NotFoundError(ERROR_MESSAGE.DELETE_LIKE_NO_ID));
